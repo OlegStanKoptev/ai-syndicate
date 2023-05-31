54004ac4a42c25fccd0366ff976c7c7313ee3ac3
Oleg Koptev <feldspar@yandex-team.ru>
2023-05-31 15:37:26 +0300

    added cookies

--- client/lib/src/constants/client.dart
+++ client/lib/src/constants/client.dart
@@ -1,6 +1,11 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 
-final dio = Dio()..options.baseUrl = 'https://feldspar.space';
+final dio = Dio()
+  ..options.baseUrl = 'https://feldspar.space'
+  ..options.contentType = 'application/json'
+  ..interceptors.add(CookieManager(CookieJar()));
 
 // Future<void> prepareJar() async {
 //   final Directory appDocDir = await getApplicationDocumentsDirectory();
