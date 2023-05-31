e9ab9ec5f22f780fd4ef0baa59783892e2838875
vaziliybober <vaziliybober@gmail.com>
2023-05-31 12:48:09 +0300

    add something for user profile for Oleg

--- server/app/routes/api/user/me.ts
+++ server/app/routes/api/user/me.ts
@@ -1,42 +1,34 @@
 import type { LoaderFunction } from "@remix-run/server-runtime";
-import { json } from "@remix-run/server-runtime";
-import invariant from "tiny-invariant";
+import { db } from "~/db.server";
 import { requireCurrentApiUser } from "~/utils.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await requireCurrentApiUser(request);
-  const commonFields = {
-    id: user.id,
-    role: user.role,
-    email: user.email,
-    avatarImageFile: user.avatarImageFile,
-  };
-  if (user.role === "user") {
-    return json({
-      ...commonFields,
-      fullName: user.fullName,
-      balance: user.balance,
-    });
-  }
-  if (user.role === "expert") {
-    return json({
-      ...commonFields,
-      fullName: user.fullName,
-    });
-  }
-  if (user.role === "developer") {
-    return json({
-      ...commonFields,
-      orgName: user.orgName,
-      shortOrgName: user.shortOrgName,
-      inn: user.inn,
-      ogrn: user.ogrn,
-      kpp: user.kpp,
-      legalAddress: user.legalAddress,
-      actualAddress: user.actualAddress,
-      website: user.website,
-      phone: user.phone,
-    });
-  }
-  invariant(false, `Unexpected role: ${user.role}`);
+  const userInfo = await db.user.findUnique({
+    where: { id: user.id },
+    include: {
+      votesReport: {
+        orderBy: { updatedAt: "desc" },
+        include: { report: { include: { startup: true } } },
+      },
+      applicationsDeveloper: {
+        orderBy: { updatedAt: "desc" },
+        include: { startup: true },
+      },
+      createdBy: true,
+      startupsCreated: { orderBy: { updatedAt: "desc" } },
+      votesNewStartup: {
+        orderBy: { updatedAt: "desc" },
+        include: { startup: true },
+      },
+      investments: {
+        orderBy: { updatedAt: "desc" },
+        include: { startup: true },
+      },
+      deposits: { orderBy: { updatedAt: "desc" } },
+      refunds: { orderBy: { updatedAt: "desc" } },
+      incomes: { orderBy: { updatedAt: "desc" } },
+    },
+  });
+  return userInfo;
 };
